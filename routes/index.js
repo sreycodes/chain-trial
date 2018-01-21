2ea8b011dda587aaf0751c82313a9359f2fafa52
sidhant <sidhbansal@gmail.com>
2018-01-21 17:49:30 +0800

    fixed res.end() in get_coords

--- routes/index.js
+++ routes/index.js
@@ -73,7 +73,7 @@ router.post('/get_coord', isLoggedIn, function(req, res, next) {
             });
             console.log("i am here");
         });
-
+    res.end();
 });
 
 router.get('/profile', isLoggedIn, function(req, res) {

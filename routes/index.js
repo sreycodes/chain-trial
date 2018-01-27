4ab345642422ab2cfc638ea3236b5fbbdead6910
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-01-28 04:36:04 +0800

    Fixed deleted problems

--- routes/index.js
+++ routes/index.js
@@ -122,7 +122,7 @@ router.get('/gameplay', isLoggedIn, function(req, res, next) {
     var new_list_users = [];
     list_users.forEach(function(user, index) {
       var dist = geodist([user.local.lat, user.local.lng], [req.user.local.lat, req.user.local.lng], {format: false, unit: 'km'});
-      if(!user.local.chain && dist <= 10 && !user._id.equals(req.user._id) && user.local.loggedIn) {
+      if((!user.local.chain || user.local.chain == "deleted") && dist <= 10 && !user._id.equals(req.user._id) && user.local.loggedIn) {
         new_list_users.push(user.local);
       }
     });
